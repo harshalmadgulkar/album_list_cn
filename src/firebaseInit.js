4eb77b99c7524a4239f35b1086bbbaf794085e16
harshalmadgulkar <harshal.madgulkar725@gmail.com>
2024-04-18 09:31:01 +0530

    Firebase Hosting

---
+++ src/firebaseInit.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7dvVlvCCy8h_vuAFBYV3_2wmD0Fbfjnc",
+  authDomain: "cn-album-list-10088.firebaseapp.com",
+  projectId: "cn-album-list-10088",
+  storageBucket: "cn-album-list-10088.appspot.com",
+  messagingSenderId: "683158740084",
+  appId: "1:683158740084:web:f8f984b755699fa09b0521"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
